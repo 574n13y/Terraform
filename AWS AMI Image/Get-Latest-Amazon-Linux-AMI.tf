38006e8e0c6ce7b06449eaf87213026e5eaf273c
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-14 18:50:03 +0530

    Create Get-Latest-Amazon-Linux-AMI.tf

---
+++ AWS AMI Image/Get-Latest-Amazon-Linux-AMI.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Get latest Amazon Linux 2 AMI
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
