f35562d028c0648c153fbbaa752d665994a3a8ca
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-14 18:50:47 +0530

    Create Get-Latest-CentOS-AMI.tf

---
+++ AWS AMI Image/Get-Latest-CentOS-AMI.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "centos-linux-6" {
+  most_recent = true
+  owners = ["679593333241"]
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 6*x86_64*"]
+  }
+}
+
+# Get latest CentOS Linux 7.x AMI
+data "aws_ami" "centos-linux-7" {
+  most_recent = true
+  owners = ["679593333241"]
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7*x86_64*"]
+  }
+}
+
+# Get latest CentOS Linux 8.x AMI
+data "aws_ami" "centos-linux-8" {
+  most_recent = true
+  owners = ["679593333241"]
+  filter {
+    name   = "name"
+    values = ["CentOS*8*x86_64*"]
+  }
+}
