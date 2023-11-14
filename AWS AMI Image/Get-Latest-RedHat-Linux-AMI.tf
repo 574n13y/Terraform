3a6bac9ae058da87f5e34af94bb1913afda7c594
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-14 18:52:48 +0530

    Create Get-Latest-RedHat-Linux-AMI.tf

---
+++ AWS AMI Image/Get-Latest-RedHat-Linux-AMI.tf
@@ -0,0 +1,29 @@
+# Get latest Red Hat Enterprise Linux 6.x AMI
+data "aws_ami" "redhat-linux-6" {
+  most_recent = true
+  owners      = ["309956199498"]
+  filter {
+    name   = "name"
+    values = ["RHEL-6.*"]
+  }
+}
+
+# Get latest Red Hat Enterprise Linux 7.x AMI
+data "aws_ami" "redhat-linux-7" {
+  most_recent = true
+  owners      = ["309956199498"]
+  filter {
+    name   = "name"
+    values = ["RHEL-7.*"]
+  }
+}
+
+# Get latest Red Hat Enterprise Linux 8.x AMI
+data "aws_ami" "redhat-linux-8" {
+  most_recent = true
+  owners      = ["309956199498"]
+  filter {
+    name   = "name"
+    values = ["RHEL-8.*"]
+  }
+}
