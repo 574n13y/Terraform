d428b4f56b797ab61ec4ac766462d6edf0e7e8d1
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-14 18:54:36 +0530

    Create Get-Latest-Windows-AMI.tf

---
+++ AWS AMI Image/Get-Latest-Windows-AMI.tf
@@ -0,0 +1,29 @@
+# Get latest Windows Server 2012R2 AMI
+data "aws_ami" "windows-2012-r2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2012-R2_RTM-English-64Bit-Base-*"]
+  }
+}
+
+# Get latest Windows Server 2016 AMI
+data "aws_ami" "windows-2016" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2016-English-Full-Base*"]
+  }
+}
+
+# Get latest Windows Server 2019 AMI
+data "aws_ami" "windows-2019" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base*"]
+  }
+}
